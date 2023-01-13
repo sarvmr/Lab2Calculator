73851ece41a5659429adb12a8ef07e4546d0f000
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-01-13 12:37:28 -0800

    created buttonDigit_Click function that appends the clicked digit button to the text box.

--- Lab2Calculator/Form1.cs
+++ Lab2Calculator/Form1.cs
@@ -12,6 +12,7 @@ namespace Lab2Calculator
 {
     public partial class FormCalculator : Form
     {
+        bool isCalculatorPowerOn = false;
         public FormCalculator()
         {
             InitializeComponent();
@@ -22,9 +23,29 @@ namespace Lab2Calculator
             if (buttonOnOff.Text == "Off")
             {
                 buttonOnOff.Text = "On";
+                isCalculatorPowerOn = true;
+
             } else
             {
                 buttonOnOff.Text = "Off";
+                isCalculatorPowerOn = false;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Appends the clicked digit button to the text box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDigit_Click(object sender, EventArgs e)
+        {
+            if (isCalculatorPowerOn)
+            {
+                Button buttonDigit = (Button)sender;
+                SendKeys.Send(buttonDigit.Text);
+                this.textBoxNumbers.Text += buttonDigit.Text;
             }
         }
     }
