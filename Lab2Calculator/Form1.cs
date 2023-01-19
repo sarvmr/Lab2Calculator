eb4baa9396bb69213eeed25a621655cfba87c1c3
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-01-18 23:13:35 -0800

    merge completed

--- Lab2Calculator/Form1.cs
+++ Lab2Calculator/Form1.cs
@@ -14,6 +14,12 @@ using System.Windows.Forms;
 /// </summary>
 namespace Lab2Calculator
 {
+    /// <summary>
+    /// Lab 2 Calculator Application
+    /// Authors: Samuel Tjahjadi A00978466 & Sarvenaz Mohammadi A01158178
+    /// Date: January 18, 2023
+    /// Source: n/a
+    /// </summary>
     public partial class FormCalculator : Form
     {
         char[] validOperationSymbols = new char[] {'+', '-', '*', '/'};
@@ -22,6 +28,8 @@ namespace Lab2Calculator
         double LastNumber;
         string Operation;
         string operationSymbol;
+
+        double memory;
 
         public FormCalculator()
         {
@@ -94,73 +102,45 @@ namespace Lab2Calculator
         }
 
         /// <summary>
-        /// function of "+" button 
+        /// Detects which arithmetic button was called and executes that arithmetic operation.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void buttonAdd_Click(object sender, EventArgs e)
+        private void buttonArithmetic_Click(object sender, EventArgs e)
         {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-            textBoxNumbers.Text = "";
-            Button addButton = (Button)sender;
-            SendKeys.Send("{ADD}");
-            Operation = "+";
+            if (isCalculatorPowerOn)
+            {
+                var isNumeric = int.TryParse(textBoxNumbers.Text, out _);
+                if (isNumeric)
+                {
+                    FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
+                    textBoxNumbers.Text = "";
 
-        }
-        /// <summary>
-        /// function of "-" button 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonSubtract_Click(object sender, EventArgs e)
-        {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-            textBoxNumbers.Text = "";
-            Button subButton = (Button)sender;
-            SendKeys.Send("{SUBTRACT}");
-            Operation = "-";
+                }
+                Button arithmeticButton = (Button)sender;
+                Operation = arithmeticButton.Text;
 
-        }
-        /// <summary>
-        /// function of "*" button 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonMultiply_Click(object sender, EventArgs e)
-        {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-            textBoxNumbers.Text = "";
-            Button mulButton = (Button)sender;
-            SendKeys.Send("{MULTIPLY}");
-            Operation = "*";
-
-        }
-        /// <summary>
-        /// function of "/" button 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonDivide_Click(object sender, EventArgs e)
-        {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-            textBoxNumbers.Text = "";
-            Button divButton = (Button)sender;
-            SendKeys.Send("{DIVIDE}");
-            Operation = "/";
-
-        }
-        /// <summary>
-        /// function of "%" button 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonModulo_Click(object sender, EventArgs e)
-        {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-            textBoxNumbers.Text = "";
-            //Button modButton = (Button)sender;
-            //SendKeys.Send("{REMAINDER}");
-            Operation = "%";
+                if (arithmeticButton.Text == "+")
+                {
+                    SendKeys.Send("{ADD}");
+                }
+                else if (arithmeticButton.Text == "-")
+                {
+                    SendKeys.Send("{SUBTRACT}");
+                }
+                else if (arithmeticButton.Text == "*")
+                {
+                    SendKeys.Send("{MULTIPLY}");
+                }
+                else if (arithmeticButton.Text == "/")
+                {
+                    SendKeys.Send("{DIVIDE}");
+                }
+                else if (arithmeticButton.Text == "%")
+                {
+                    SendKeys.Send("{MOD}");
+                }
+            }
 
         }
         /// <summary>
@@ -193,7 +173,10 @@ namespace Lab2Calculator
         private void buttonSqrt_Click(object sender, EventArgs e)
         {
             FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-
+            if (textBoxNumbers.Text == "")
+            {
+                FirstNumber = Convert.ToDouble(textBoxResult.Text);
+            }
             if (FirstNumber < 0)
             {
                 textBoxResult.Text = "No square root for negative numbers";
@@ -204,6 +187,7 @@ namespace Lab2Calculator
                 double Result = Math.Sqrt(FirstNumber);
                 textBoxResult.Text = Convert.ToString(Result);
                 FirstNumber = Result;
+                textBoxNumbers.Text = "";
             }
         }
 
@@ -214,11 +198,18 @@ namespace Lab2Calculator
         /// <param name="e"></param>
         private void buttonPowerTwo_Click(object sender, EventArgs e)
         {
-            FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
-
-                double Result = Math.Pow(FirstNumber , 2.0);
+           
+            if (textBoxNumbers.Text == "")
+            {
+                FirstNumber = Convert.ToDouble(textBoxResult.Text);
+            } else
+            {
+                FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
+            }
+            double Result = Math.Pow(FirstNumber , 2.0);
                 textBoxResult.Text = Convert.ToString(Result);
                 FirstNumber = Result;
+            textBoxNumbers.Text = "";
         }
         
 
@@ -267,7 +258,6 @@ namespace Lab2Calculator
                     FirstNumber = Result;
                 }
             }
-
             if (Operation == "%")
             {
                 if (SecondNumber == 0)
@@ -282,7 +272,78 @@ namespace Lab2Calculator
                     FirstNumber = Result;
                 }
             }
+            textBoxNumbers.Text = "";
+        }
 
+        /// <summary>
+        /// Memory Store puts the number on the display into the memory
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonMemoryFunctions_Click(object sender, EventArgs e)
+        {
+            if (isCalculatorPowerOn)
+            {
+                Button memoryButton = (Button)sender;
+                if (memoryButton.Text == "MS") // Memory Store: Stores number into memory
+                {
+                    if (textBoxNumbers.Text == "") 
+                    {
+                        if (textBoxResult.Text != "")
+                        {
+                            memory = Convert.ToDouble(textBoxResult.Text);
+                        }
+                    } else
+                    {
+                        memory = Convert.ToDouble(textBoxNumbers.Text);
+                    }
+                }
+                else if (memoryButton.Text == "MR") //  Memory Recall: Displays memory onto screen
+                {
+                    textBoxNumbers.Text = Convert.ToString(memory);
+                }
+                else if (memoryButton.Text == "MC") // Memory Clear: Clears memory
+                {
+                    memory = 0;
+                } else if (memoryButton.Text == "M+") // Memory Plus: Adds number to the memory
+                {
+                    if (textBoxNumbers.Text == "")
+                    {
+                        if (textBoxResult.Text != "")
+                        {
+                            memory += Convert.ToDouble(textBoxResult.Text);
+                        }
+                    } else
+                    {
+                        memory += Convert.ToDouble(textBoxNumbers.Text);
+                    }
+                }
+
+
+            }
+        }
+
+        private void buttonSignChange_Click(object sender, EventArgs e)
+        {
+            if (isCalculatorPowerOn)
+            {
+                if (textBoxNumbers.Text != "")
+                {
+                    textBoxNumbers.Text = Convert.ToString(-1.0 * Convert.ToDouble(textBoxNumbers.Text));
+                } else if (textBoxResult.Text != "")
+                {
+                    textBoxResult.Text = Convert.ToString(-1.0 * Convert.ToDouble(textBoxResult.Text));
+
+                }
+            }
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            if (isCalculatorPowerOn)
+            {
+                textBoxNumbers.Text = "0";
+            }
         }
 
         /// <summary>
@@ -295,7 +356,7 @@ namespace Lab2Calculator
             if (isCalculatorPowerOn)
             {
                 ///checks if the input is only numbers
-                if (char.IsDigit(e.KeyChar) || e.KeyChar == '.') 
+                if (char.IsDigit(e.KeyChar) || e.KeyChar == '.')
                 {
                     e.Handled = true;
                     /// if first time typing or after pressing operation buttons, clears the text box
@@ -305,7 +366,7 @@ namespace Lab2Calculator
                         textBoxNumbers.Text = "";
                     }
                     textBoxNumbers.Text += e.KeyChar;
-                    
+
                     FirstNumber = Convert.ToDouble(textBoxNumbers.Text);
                 }
                 /// checks if you are doing the main operations
@@ -321,7 +382,7 @@ namespace Lab2Calculator
                     switch (operationSymbol)
                     {
                         case "+":
-                        textBoxResult.Text = Convert.ToString(FirstNumber + LastNumber);
+                            textBoxResult.Text = Convert.ToString(FirstNumber + LastNumber);
                             break;
 
                         case "-":
@@ -352,4 +413,7 @@ namespace Lab2Calculator
         }
     }
 }
+
+
+
 
